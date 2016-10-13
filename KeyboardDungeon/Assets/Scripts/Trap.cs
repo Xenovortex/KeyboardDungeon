ab6f7a794c9a09c80fe7f1a9cdb410eb3e8b3ac8
kangsoontan <tankangsoon@gmail.com>
2016-10-13 20:35:43 +0800

    kang soon's scene with very basic framework for trap

---
+++ KeyboardDungeon/Assets/Scripts/Trap.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trap : MonoBehaviour {
+
+	public KeyCode key;
+
+	void Update () {
+		if (Input.GetKeyDown (key)) {
+			this.GetComponent<Renderer> ().material.color = Color.green;
+		}
+	}
+}
