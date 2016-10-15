08c5201ceeba0c1dab48ae300e1ff863c20c9061
The-Gia Leo Nguyen <Leo.Nguyen@gmx.de>
2016-10-16 05:19:16 +0800

    Add Rotation to Enemy Movement
    The Enemy now always faces the direction of movement.

--- KeyboardDungeon/Assets/Leo_testing stuff/Scripts/MoveEnemy.cs
+++ KeyboardDungeon/Assets/Leo_testing stuff/Scripts/MoveEnemy.cs
@@ -10,13 +10,15 @@ public class MoveEnemy : MonoBehaviour
 	private float lastWaypointSwitchTime;				// Time at which the last waypoint was passed
 	public float speed = 1.0f;							// Speed at which the enemy is moving 
 
+
 	// Use this for initialization
 	void Start ()
 	{
 		// Initialize to current time
 		lastWaypointSwitchTime = Time.time;
 	}
-	
+
+
 	// Update is called once per frame
 	void Update () 
 	{
@@ -24,6 +26,7 @@ public class MoveEnemy : MonoBehaviour
 		Vector3 startPosition = waypoints [currentWaypoint].transform.position;
 		Vector3 endPostion = waypoints [currentWaypoint + 1].transform.position;
 
+		// Makes enemy walk along the path segment according to the defined speed
 		float pathLength = Vector3.Distance (startPosition, endPostion);				// Calculate length of the path segment 
 		float totalTimeForPath = pathLength / speed;									// Calculate time for which the enemy should cover the path segment
 		float currentTimeOnPath = Time.time - lastWaypointSwitchTime;					// Calculate the time passed since the enemy started to move on the current path segment
@@ -32,13 +35,15 @@ public class MoveEnemy : MonoBehaviour
 		// Check if enemy has reached the last waypoint, if not update to new path segment otherwise destroy enemy and inflict damage...(in progress)
 		if (gameObject.transform.position.Equals (endPostion))
 		{
-			// Update to new path segment
+			// Update to new path segment and rotate enemy to movement direction
 			if (currentWaypoint < waypoints.Length - 2)
 			{
+				// Update to new path segment
 				currentWaypoint++;
 				lastWaypointSwitchTime = Time.time;
 
-				// Rotate enemy to move direction (ToDo), NEED BETTER IMAGE FOR ENEMY!!! Top-Down preferred. 
+				// Rotate enemy to movement direction
+				RotateIntoMoveDirection ();
 			}
 			else 
 			{
@@ -53,5 +58,25 @@ public class MoveEnemy : MonoBehaviour
 			}
 		}
 
+	}
+
+
+	// Define function to rotate the enemy accordingly to the movement direction
+	private void RotateIntoMoveDirection ()
+	{
+		// Initialize start- and end-waypoint for the current path segment and determine current movement direction
+		Vector3 newStartPosition = waypoints [currentWaypoint].transform.position;
+		Vector3 newEndPostion = waypoints [currentWaypoint + 1].transform.position;
+		Vector3 newDirection = (newEndPostion - newStartPosition);
+
+		// Calculate angle between the current movement direction and the x-axis
+		float x = newDirection.x;
+		float z = newDirection.z;
+		float rotationAngle = Mathf.Atan2 (z, x) * (180 / Mathf.PI);
+
+		// Rotates the child Sprite according to the above calculated angle along the y-axis 
+		GameObject sprite = (GameObject)
+			gameObject.transform.FindChild ("Sprite").gameObject;
+		sprite.transform.rotation = Quaternion.Euler (90, 0, rotationAngle);
 	}
 }
